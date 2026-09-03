ee755a354f75755a0da0ebf2e947de53eaf5b1cb
law0426 <law0426law@gmail.com>
2026-09-03 10:21:44 +0200

    Initial Commit

---
+++ Core/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Core;
+
+public class Book
+{
+
+    public string Title{get;}
+    public bool Borrowed{get; private set;}
+
+    public Book(string title){
+        Title = title;
+        Borrowed = false;
+    }
+
+    // public Borrow()
+    // {
+    //     if(Borrowed == tr)
+    // }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Book_CanBeAded_AndBorrowed()
+    {
+        //Arrange
+        List<Book> books = new List<Book>();
+        books.Add(new Book("Book1"));
+        //Act
+        
+        //Assert.
+
+    }
+}
